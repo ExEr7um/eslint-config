c6cec42c0ecfd2f0256c61c7b42d9281562e84c0
Vasily Kuzin <exer7um@gmail.com>
2024-03-27 18:00:11 +0300

    feat!: Обновление правил Vue (#4)
    ### Добавлены правила
    - `vue/define-emits-declaration`
    - `vue/define-macros-order`
    - `vue/define-props-declaration`
    - `vue/html-button-has-type`
    - `vue/html-comment-content-newline`
    - `vue/html-comment-content-spacing`
    - `vue/html-comment-indent`
    - `vue/no-ref-object-reactivity-loss`
    - `vue/padding-line-between-blocks`
    - `vue/prefer-true-attribute-shorthand`
    - `vue/require-typed-ref`
    - `vue/v-bind-style`
    - `vue/v-for-delimiter-style`

--- index.js
+++ index.js
@@ -88,15 +88,40 @@ export default [
       ],
       "vue/block-tag-newline": "error",
       "vue/component-api-style": ["error", ["script-setup"]],
+      "vue/define-emits-declaration": "error",
+      "vue/define-macros-order": [
+        "error",
+        {
+          order: ["defineProps", "defineEmits", "defineModel"],
+          defineExposeLast: true,
+        },
+      ],
+      "vue/define-props-declaration": "error",
+      "vue/html-button-has-type": "error",
+      "vue/html-comment-content-newline": "error",
+      "vue/html-comment-content-spacing": "error",
+      "vue/html-comment-indent": "error",
       "vue/multi-word-component-names": "off",
+      "vue/no-ref-object-reactivity-loss": "error",
       "vue/no-setup-props-reactivity-loss": "error",
       "vue/no-unused-refs": "error",
+      "vue/padding-line-between-blocks": "error",
+      "vue/prefer-true-attribute-shorthand": "error",
       "vue/require-prop-comment": [
         "error",
         {
           type: "JSDoc",
         },
       ],
+      "vue/require-typed-ref": "error",
+      "vue/v-bind-style": [
+        "error",
+        "shorthand",
+        {
+          sameNameShorthand: "always",
+        },
+      ],
+      "vue/v-for-delimiter-style": "error",
     },
   },
 
