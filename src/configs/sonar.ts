5d5415b82b11c7f8f57eed31a9331c7a28311cf0
Vasily Kuzin <work@exer7um.com>
2025-03-04 19:02:27 +0300

    feat: Обновление `eslint-plugin-sonarjs` до `v3` (#17)

--- src/configs/sonar.ts
+++ src/configs/sonar.ts
@@ -3,8 +3,14 @@ import type { Linter } from "eslint"
 import { configs as sonarConfigs } from "eslint-plugin-sonarjs"
 
 export default [
-  // @ts-expect-error - неправильная типизация
   sonarConfigs.recommended,
 
-  { name: "sonar/base", rules: { "sonarjs/no-duplicate-string": "off" } },
+  {
+    name: "sonar/base",
+    rules: {
+      "sonarjs/fixme-tag": "off",
+      "sonarjs/no-duplicate-string": "off",
+      "sonarjs/todo-tag": "off",
+    },
+  },
 ] as const satisfies Linter.Config[]
