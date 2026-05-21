bf27d414e7d48c0cc2d07095a481ec60eabe5777
Vasily Kuzin <work@exer7um.com>
2026-05-21 12:29:43 +0300

    fix: Обновление `eslint-plugin-jsonc` до `v3`

--- src/configs/jsonc.ts
+++ src/configs/jsonc.ts
@@ -3,27 +3,11 @@ import type { Linter } from "eslint"
 import { configs as jsoncConfigs } from "eslint-plugin-jsonc"
 
 export default [
-  ...jsoncConfigs["flat/recommended-with-json"],
-  ...jsoncConfigs["flat/prettier"],
+  ...jsoncConfigs["recommended-with-json"],
+  ...jsoncConfigs.prettier,
 
   {
-    files: [
-      "*.json",
-      "**/*.json",
-      "*.json5",
-      "**/*.json5",
-      "*.jsonc",
-      "**/*.jsonc",
-      "**/*.vue",
-    ],
-    ignores: [
-      "**/.nuxt",
-      "**/.output",
-      "**/dist",
-      "**/node_modules",
-      "coverage/**",
-      "**/package.json",
-    ],
+    ignores: ["**/package.json"],
     name: "json/sort-keys",
     rules: { "jsonc/sort-keys": ["error", "asc"] },
   },
